ee9a6251352d77fba33eff4bd194c81e0838dcd0
Miguel Campos <miguel@gwenyth.io>
2023-05-26 00:15:36 -0700

    fix no-op project

--- libs/apps/sys/src/lib/noop.ts
+++ libs/apps/sys/src/lib/noop.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import { newMethod } from '@worksheets/apps/framework';
 
 export const noop = newMethod({
+  path: 'no.op',
   label: 'No Op',
   description: 'Accepts any inputs and does nothing.',
   input: z.any(),
