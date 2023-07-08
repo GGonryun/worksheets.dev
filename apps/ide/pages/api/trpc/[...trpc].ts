497043db3e7e69add722c2604cf41e8fae3633af
Miguel Campos <miguel@gwenyth.io>
2023-07-07 23:26:56 -0700

    revert error reporting changes

--- apps/ide/pages/api/trpc/[...trpc].ts
+++ apps/ide/pages/api/trpc/[...trpc].ts
@@ -1,18 +1,19 @@
 import * as trpcNext from '@trpc/server/adapters/next';
 import { getHTTPStatusCodeFromError } from '@trpc/server/http';
-import { logger } from '@worksheets/feat/logging';
+import { errors } from '@worksheets/feat/error-reporting';
 import { appRouter, createContext } from '@worksheets/trpc/ide/server';
-
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: createContext,
   onError(opts) {
-    const { error, type, req, path } = opts;
-    const ip = (req.headers['x-forwarded-for'] ??
-      req.headers['x-real-ip'] ??
-      undefined) as string | undefined;
-    const url = req.url;
-    const status = getHTTPStatusCodeFromError(error);
-    logger.error(error, 'trpc error', { type, ip, url, status, path });
+    const { error, type, req } = opts;
+    errors.report(error, {
+      statusCode: getHTTPStatusCodeFromError(error),
+      url: req.url,
+      remoteAddress: (req.headers['x-forwarded-for'] ??
+        req.headers['x-real-ip'] ??
+        undefined) as string | undefined,
+      method: type,
+    });
   },
 });
