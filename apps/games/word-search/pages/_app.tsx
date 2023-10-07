66d457254e602de2d7432ea907e0ef8d974d5bd1
Miguel Campos <miguel@gwenyth.io>
2023-10-06 17:54:54 -0700

    add fullstory to wordsearch

--- apps/games/word-search/pages/_app.tsx
+++ apps/games/word-search/pages/_app.tsx
@@ -2,13 +2,21 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
 import { useVersion } from '@worksheets/ui-core';
+import * as FullStory from '@fullstory/browser';
 import { APP_VERSION } from '../util';
 import { UpdateGameModal } from '../components/UpdateGameModal';
-import { SERVER_SETTINGS } from '@worksheets/data-access/server-settings';
+import {
+  SERVER_SETTINGS,
+  SERVICE_SETTINGS,
+} from '@worksheets/data-access/server-settings';
 import { verifier } from '../puzzles/verifier';
 
 if (!SERVER_SETTINGS.ENVIRONMENT.IS_PRODUCTION()) {
   verifier();
+}
+
+if (typeof window !== 'undefined') {
+  FullStory.init(SERVICE_SETTINGS.FULLSTORY);
 }
 
 function CustomApp({ Component, pageProps }: AppProps) {
