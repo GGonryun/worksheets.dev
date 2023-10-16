fc0025abb344877ca7b82b6e9ea0255ca5d1c077
Miguel Campos <miguel@gwenyth.io>
2023-10-15 23:55:11 -0700

    try to remove touch move listener

--- apps/games/nonograms/pages/_app.tsx
+++ apps/games/nonograms/pages/_app.tsx
@@ -4,7 +4,7 @@ import './styles.css';
 import * as FullStory from '@fullstory/browser';
 import { MobileMeta, UpdateGameModal } from '@worksheets/ui-games';
 import { SERVICE_SETTINGS } from '@worksheets/data-access/server-settings';
-import { useVersion } from '@worksheets/ui-core';
+import { useEventListener, useVersion } from '@worksheets/ui-core';
 import { APP_VERSION, GAME_TITLE } from '../util/constants';
 import { useRouter } from 'next/router';
 
@@ -20,11 +20,20 @@ function CustomApp({ Component, pageProps }: AppProps) {
     update();
     reload();
   };
+
+  useEventListener('touchmove', (e) => e.preventDefault(), undefined, {
+    passive: false,
+  });
+
   return (
     <>
       <Head>
         <title>{GAME_TITLE}</title>
         <MobileMeta />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
+        />
       </Head>
       <main>
         <Component {...pageProps} />
