30d191566c5b6ab789ec474b98526721de5b9321
Miguel Campos <miguel@gwenyth.io>
2023-10-28 20:37:30 -0700

    prisma: add verification property on emails

--- libs/prisma/src/zod/email.ts
+++ libs/prisma/src/zod/email.ts
@@ -1,8 +1,9 @@
-import * as z from 'zod';
+import * as z from "zod"
 
 export const EmailModel = z.object({
   id: z.string(),
   address: z.string(),
+  verified: z.boolean(),
   createdAt: z.date(),
   updatedAt: z.date(),
-});
+})
