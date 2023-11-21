03e94bdcae1045194062250ccb3c5a12f03a8bf3
Miguel Campos <amodestduck@gmail.com>
2023-11-20 22:40:54 -0800

    separate layout content

--- libs/ui/charity/src/lib/layout/layout.tsx
+++ libs/ui/charity/src/lib/layout/layout.tsx
@@ -1,17 +1,19 @@
-import * as React from 'react';
 import Box from '@mui/material/Box';
 
 import MuiToolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+
 import { Toolbar } from './toolbar';
 import { Drawer } from './drawer/drawer';
+import { GameRecommendations } from './drawer/game-recommendations';
+import { useState } from 'react';
 
 type LayoutProps = {
-  // TODO
+  // TODO: add props
+  children: React.ReactNode;
 };
 
-export const Layout: React.FC<LayoutProps> = () => {
-  const [open, setOpen] = React.useState(false);
+export const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const [open, setOpen] = useState(false);
 
   const handleDrawerToggle = () => {
     setOpen((prevState) => !prevState);
@@ -20,46 +22,14 @@ export const Layout: React.FC<LayoutProps> = () => {
   return (
     <Box sx={{ display: 'flex' }}>
       <Toolbar onDrawerToggle={handleDrawerToggle} connected={false} />
-      <Drawer onDrawerToggle={handleDrawerToggle} open={open} />
+      <Drawer
+        onDrawerToggle={handleDrawerToggle}
+        open={open}
+        children={<GameRecommendations />}
+      />
       <Box component="main">
         <MuiToolbar />
-        <Typography>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-          unde fugit veniam eius, perspiciatis sunt? Corporis qui ducimus
-          quibusdam, aliquam dolore excepturi quae. Distinctio enim at eligendi
-          perferendis in cum quibusdam sed quae, accusantium et aperiam? Quod
-          itaque exercitationem, at ab sequi qui modi delectus quia corrupti
-          alias distinctio nostrum. Minima ex dolor modi inventore sapiente
-          necessitatibus aliquam fuga et. Sed numquam quibusdam at officia
-          sapiente porro maxime corrupti perspiciatis asperiores, exercitationem
-          eius nostrum consequuntur iure aliquam itaque, assumenda et! Quibusdam
-          temporibus beatae doloremque voluptatum doloribus soluta accusamus
-          porro reprehenderit eos inventore facere, fugit, molestiae ab officiis
-          illo voluptates recusandae. Vel dolor nobis eius, ratione atque
-          soluta, aliquam fugit qui iste architecto perspiciatis. Nobis,
-          voluptatem! Cumque, eligendi unde aliquid minus quis sit debitis
-          obcaecati error, delectus quo eius exercitationem tempore. Delectus
-          sapiente, provident corporis dolorum quibusdam aut beatae repellendus
-          est labore quisquam praesentium repudiandae non vel laboriosam quo ab
-          perferendis velit ipsa deleniti modi! Ipsam, illo quod. Nesciunt
-          commodi nihil corrupti cum non fugiat praesentium doloremque
-          architecto laborum aliquid. Quae, maxime recusandae? Eveniet dolore
-          molestiae dicta blanditiis est expedita eius debitis cupiditate porro
-          sed aspernatur quidem, repellat nihil quasi praesentium quia eos,
-          quibusdam provident. Incidunt tempore vel placeat voluptate iure
-          labore, repellendus beatae quia unde est aliquid dolor molestias
-          libero. Reiciendis similique exercitationem consequatur, nobis placeat
-          illo laudantium! Enim perferendis nulla soluta magni error, provident
-          repellat similique cupiditate ipsam, et tempore cumque quod! Qui, iure
-          suscipit tempora unde rerum autem saepe nisi vel cupiditate iusto.
-          Illum, corrupti? Fugiat quidem accusantium nulla. Aliquid inventore
-          commodi reprehenderit rerum reiciendis! Quidem alias repudiandae eaque
-          eveniet cumque nihil aliquam in expedita, impedit quas ipsum nesciunt
-          ipsa ullam consequuntur dignissimos numquam at nisi porro a, quaerat
-          rem repellendus. Voluptates perspiciatis, in pariatur impedit, nam
-          facilis libero dolorem dolores sunt inventore perferendis, aut
-          sapiente modi nesciunt.
-        </Typography>
+        {children}
       </Box>
     </Box>
   );
