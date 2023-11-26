30335c3e3a6052b06d5a74a2f4701dc3837c323b
Miguel Campos <amodestduck@gmail.com>
2023-11-25 21:38:19 -0800

    add social icons

---
+++ libs/ui/charity/src/lib/icons/steam-games.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta } from '@storybook/react';
+import { SteamGames } from './steam-games';
+
+const Story: Meta<typeof SteamGames> = {
+  component: SteamGames,
+  title: 'Icons/SteamGames',
+};
+export default Story;
+
+export const Primary = {
+  args: {
+    fontSize: 'large',
+  },
+};
+
+export const Large = {
+  args: {
+    sx: {
+      height: 200,
+      width: 200,
+    },
+  },
+};
---
+++ libs/ui/charity/src/lib/icons/tiktok.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta } from '@storybook/react';
+import { TikTok } from './tiktok';
+
+const Story: Meta<typeof TikTok> = {
+  component: TikTok,
+  title: 'Icons/TikTok',
+};
+export default Story;
+
+export const Primary = {
+  args: {
+    fontSize: 'large',
+  },
+};
+
+export const Large = {
+  args: {
+    sx: {
+      height: 200,
+      width: 200,
+    },
+  },
+};
