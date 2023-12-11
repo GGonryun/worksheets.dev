5a109c0b7b7a28af9b5903a27f8f041371f3b618
Miguel Campos <amodestduck@gmail.com>
2023-12-10 21:21:31 -0800

    change application type for sitemap

--- apps/charity/pages/api/sitemap.ts
+++ apps/charity/pages/api/sitemap.ts
@@ -101,7 +101,7 @@ const addDevelopers = () =>
 
 const handler: NextApiHandler = (req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/xml');
+  res.setHeader('Content-Type', 'application/xml');
 
   // Instructing the Vercel edge to cache the file
   res.setHeader('Cache-control', 'stale-while-revalidate, s-maxage=3600');
