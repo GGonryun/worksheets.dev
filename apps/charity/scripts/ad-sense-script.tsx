8accfb7409e1154e8f2650a50292ed05392395e7
Miguel Campos <amodestduck@gmail.com>
2023-12-17 21:41:23 -0800

    undo script modification

--- apps/charity/scripts/ad-sense-script.tsx
+++ apps/charity/scripts/ad-sense-script.tsx
@@ -1,15 +1,17 @@
 import Script from 'next/script';
 
-export const AdSenseScript = () => (
-  <Script
-    async
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1305399541775198"
-    crossOrigin="anonymous"
-  />
-);
+export const AdSenseScript = () => {
+  return (
+    <Script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1305399541775198"
+      crossOrigin="anonymous"
+    />
+  );
+};
 
-export const AdsensePushScript = () => (
+export const AdsensePushScript = () => {
   <Script id="adsense-push">
-    {`var adsbygoogle; if (adsbygoogle && !adsbygoogle.loaded) (adsbygoogle = window.adsbygoogle || []).push({});`}
-  </Script>
-);
+    {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+  </Script>;
+};
