fa049efb432642ff2fdbbdf16876657da253a7d8
Miguel Campos <amodestduck@gmail.com>
2023-12-17 22:09:07 -0800

    fix adsense push script

--- apps/charity/scripts/ad-sense-script.tsx
+++ apps/charity/scripts/ad-sense-script.tsx
@@ -1,17 +1,15 @@
 import Script from 'next/script';
 
-export const AdSenseScript = () => {
-  return (
-    <Script
-      async
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1305399541775198"
-      crossOrigin="anonymous"
-    />
-  );
-};
+export const AdSenseScript = () => (
+  <Script
+    async
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1305399541775198"
+    crossOrigin="anonymous"
+  />
+);
 
-export const AdsensePushScript = () => {
+export const AdsensePushScript = () => (
   <Script id="adsense-push">
     {`(adsbygoogle = window.adsbygoogle || []).push({});`}
-  </Script>;
-};
+  </Script>
+);
