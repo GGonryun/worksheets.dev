12bd8588d40216e0a343659896dfb8b8efb25736
Miguel Campos <amodestduck@gmail.com>
2023-12-29 09:11:49 -0800

    update our mission statement

--- libs/ui/pages/about/src/lib/about-screen.tsx
+++ libs/ui/pages/about/src/lib/about-screen.tsx
@@ -26,24 +26,27 @@ export const AboutScreen: FC<AboutScreenProps> = (props) => {
         <br />
         <Typography variant="h4">Our Mission</Typography>
         <Typography>
-          Charity.Games is an online arcade dedicated to creating fun and free
-          browser games that help support charitable causes. At Charity.Games we
-          believe that everyone should have access to clean water. We also
-          believe that everyone should have access to fun games. We combine
-          these two ideas to create a platform that allows you to play fun games
-          while also helping people in need. Every game you play on our platform
-          generates water for people in need. We currently support{' '}
+          Charity Games is a community of developers, designers, artists,
+          musicians, and gamers united by a common goal: using the power of
+          gaming to create a positive impact on the world.
+          <br />
+          <br />
+          Charity Games has built an online arcade dedicated to creating fun and
+          free browser games that help support charitable causes. We believe
+          that everyone should have access to clean water. We also believe that
+          everyone should have access to fun games. We combine these two ideas
+          to create a platform that allows you to play fun games while also
+          helping people in need. Every game you play on our platform generates
+          water for people in need. We currently support{' '}
           <Link href={urls.external.waterOrg}>Water.Org</Link> as our charity of
           choice. Read more about our{' '}
           <Link href="/charity">current donation campaign here</Link>.
-        </Typography>
-        <br />
-        <Typography>
+          <br />
+          <br />
           If you have a charity you would like to see us support, please{' '}
           <Link href={'/contact'}>contact us.</Link>
         </Typography>
         <br />
-
         <Typography variant="h4">The Team</Typography>
         <Typography variant="h6" id={blogAuthors['miguel-campos'].id}>
           Miguel Campos - Software Engineer
@@ -60,9 +63,8 @@ export const AboutScreen: FC<AboutScreenProps> = (props) => {
           <Link href={urls.external.usd}>University of San Diego</Link>, and{' '}
           <Link href={urls.external.navwar}>NAVWAR</Link> as a Software
           Engineer.
-        </Typography>
-        <br />
-        <Typography>
+          <br />
+          <br />
           When Miguel is not working, he enjoys rock climbing, hiking, and
           playing board games with his friends. Miguel really hates writing
           about himself in the third person, but he is doing it anyway.
@@ -73,12 +75,11 @@ export const AboutScreen: FC<AboutScreenProps> = (props) => {
           author="Ryunosuke Satoro"
         />
         <br />
-        <br />
         <Box>
           <Button
             variant="contained"
             color="error"
-            size="large"
+            size="small"
             startIcon={<LinkedIn sx={{ height: 30, width: 30 }} />}
             href={urls.social.linkedIn}
             sx={{
