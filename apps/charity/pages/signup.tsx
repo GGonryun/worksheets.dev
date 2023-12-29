dfb4036f2f04e22093ebaff35d0ce112a0b705b1
Miguel Campos <amodestduck@gmail.com>
2023-12-29 10:39:08 -0800

    no more signup page

--- apps/charity/pages/signup.tsx
+++ apps/charity/pages/signup.tsx
@@ -1,17 +1,12 @@
 import { NextPageWithLayout } from '@worksheets/util-next';
-import { SignUpScreen } from '@worksheets/ui/pages/login';
 import { LayoutContainer } from '../containers/layout-container';
-import { signIn } from 'next-auth/react';
 import { NextSeo } from 'next-seo';
+import { UnderConstruction } from '@worksheets/ui/pages/under-construction';
 
 const Page: NextPageWithLayout = () => (
   <>
     <NextSeo noindex={true} />
-    <SignUpScreen
-      onGoogleAction={() => signIn('google', { callbackUrl: '/' })}
-      onDiscordAction={() => signIn('discord', { callbackUrl: '/' })}
-      onGithubAction={() => signIn('github', { callbackUrl: '/' })}
-    />
+    <UnderConstruction />
   </>
 );
 
