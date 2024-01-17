29a9637c394d488e69e551a1c18c7dca2d52d56a
Miguel Campos <amodestduck@gmail.com>
2024-01-17 13:34:13 -0800

    undo url changes

--- libs/util/auth/src/lib/auth-options.ts
+++ libs/util/auth/src/lib/auth-options.ts
@@ -1,37 +1,18 @@
 import { prisma } from '@worksheets/prisma';
 import {
-  COOKIE_DOMAIN,
   DISCORD_CLIENT_ID,
   DISCORD_CLIENT_SECRET,
   GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET,
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
-  IS_DEVELOPMENT,
 } from '@worksheets/services/environment';
-import { BASE_URL } from '@worksheets/ui/env';
 import { AuthOptions } from 'next-auth';
 import DiscordProvider from 'next-auth/providers/discord';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
 import { customPrismaAdapter } from './prisma-adapter';
-
-const localSessionToken = {
-  name: `next-auth.session-token`,
-  options: {
-    httpOnly: true,
-    sameSite: 'lax' as const,
-    path: '/',
-    // support subdomains
-    domain: `${COOKIE_DOMAIN}`,
-  },
-};
-
-const productionSessionToken = {
-  name: `next-auth.session-token`,
-  options: {},
-};
 
 export const AUTH_OPTIONS: AuthOptions = {
   session: { strategy: 'jwt', maxAge: 3000 },
@@ -88,10 +69,6 @@ export const AUTH_OPTIONS: AuthOptions = {
     }),
     // ...add more providers here
   ],
-  cookies: {
-    sessionToken: productionSessionToken,
-  },
-
   callbacks: {
     async redirect(opt) {
       console.log(`redirecting user`, opt);
