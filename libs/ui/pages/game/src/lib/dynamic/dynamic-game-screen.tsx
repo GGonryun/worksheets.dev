0538d067b4ad63a8d555f31b080e507125976915
Miguel Campos <amodestduck@gmail.com>
2024-01-29 21:41:43 -0800

    add game loading screen

--- libs/ui/pages/game/src/lib/dynamic/dynamic-game-screen.tsx
+++ libs/ui/pages/game/src/lib/dynamic/dynamic-game-screen.tsx
@@ -1,8 +1,10 @@
+import { LoadingScreen } from '@worksheets/ui/pages/loading';
 import dynamic from 'next/dynamic';
 
 export const DynamicGameScreenContainer = dynamic(
   () => import('../containers/game-screen-container'),
   {
     ssr: false,
+    loading: () => <LoadingScreen />,
   }
 );
