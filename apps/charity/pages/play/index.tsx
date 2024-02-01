abccfe37e8d51fd1c2c07d225743b916e5e44530
Miguel Campos <amodestduck@gmail.com>
2024-02-01 00:55:48 -0800

    reimplement servser side props

--- apps/charity/pages/play/index.tsx
+++ apps/charity/pages/play/index.tsx
@@ -1,6 +1,7 @@
 import { DynamicLayout } from '@worksheets/ui/layout';
 import { DynamicArcadeScreen } from '@worksheets/ui/pages/arcade';
 import { NextPageWithLayout } from '@worksheets/util-next';
+import { GetServerSideProps } from 'next';
 import { NextSeo } from 'next-seo';
 
 import { gamesSeo } from '../../util/seo';
@@ -13,6 +14,12 @@ const Page: NextPageWithLayout = () => {
     </>
   );
 };
+
+export const getServerSideProps = (async (ctx) => {
+  return {
+    props: {},
+  };
+}) satisfies GetServerSideProps;
 
 Page.getLayout = (page) => {
   return <DynamicLayout>{page}</DynamicLayout>;
