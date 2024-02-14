0fd9845fa9b38acb4600efb70245350a0de19fd0
Miguel Campos <amodestduck@gmail.com>
2024-02-14 12:03:06 -0800

    custom 500 page

---
+++ apps/charity/pages/500.tsx
@@ -0,0 +1,15 @@
+import { ErrorScreen } from '@worksheets/ui/pages/errors';
+import { NextSeo } from 'next-seo';
+
+export default function Custom404() {
+  return (
+    <>
+      <NextSeo noindex title="Charity Games - 500" />
+      <ErrorScreen
+        title="500"
+        header="Internal Server Error"
+        message="Something unexpected happened on our end and our team has been notified. If the problem persists, please contact us."
+      />
+    </>
+  );
+}
