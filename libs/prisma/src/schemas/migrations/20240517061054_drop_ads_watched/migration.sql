aabb8e7612b57d70e49e3b00b468c6eaaacb17a0
Miguel Campos <amodestduck@gmail.com>
2024-05-16 23:11:23 -0700

    drop ads watched column from raffles

---
+++ libs/prisma/src/schemas/migrations/20240517061054_drop_ads_watched/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `adsWatched` on the `RaffleParticipation` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "RaffleParticipation" DROP COLUMN "adsWatched";
