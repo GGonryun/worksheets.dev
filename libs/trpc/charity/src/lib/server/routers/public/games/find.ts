4a5affe00a4c5a1c6ea645a6a9cec9f2b5577011
Miguel Campos <amodestduck@gmail.com>
2024-05-20 23:56:04 -0700

    fix: allow players to access unpublished games for static generation and testing

--- libs/trpc/charity/src/lib/server/routers/public/games/find.ts
+++ libs/trpc/charity/src/lib/server/routers/public/games/find.ts
@@ -25,7 +25,6 @@ export default publicProcedure
     const game = await db.game.findFirst({
       where: {
         id: gameId,
-        status: 'PUBLISHED',
       },
       include: {
         developer: true,
