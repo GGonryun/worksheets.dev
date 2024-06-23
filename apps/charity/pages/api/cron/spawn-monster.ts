38076017b133e301823d88116ee1592ae9acb7b8
Miguel Campos <amodestduck@gmail.com>
2024-06-23 15:51:11 -0700

    spawn: do not send boss spawn notifications

--- apps/charity/pages/api/cron/spawn-monster.ts
+++ apps/charity/pages/api/cron/spawn-monster.ts
@@ -1,11 +1,9 @@
 import { prisma } from '@worksheets/prisma';
 import { routes } from '@worksheets/routes';
 import { MobsService } from '@worksheets/services/mobs';
-import { NotificationsService } from '@worksheets/services/notifications';
 import { createCronJob } from '@worksheets/util/cron';
 
 export default createCronJob(async (_, res) => {
-  const notifications = new NotificationsService(prisma);
   const mobs = new MobsService(prisma);
   const spawned = await mobs.spawnMonster();
 
@@ -14,9 +12,10 @@ export default createCronJob(async (_, res) => {
   }
   console.info(`${spawned.mobName} has spawned!`);
 
-  if (spawned.type === 'BOSS') {
-    await notifications.send('new-battle', spawned);
-  }
+  // TODO: bosses spawn too often, reduce verbosity
+  // if (spawned.type === 'BOSS') {
+  //   await notifications.send('new-battle', spawned);
+  // }
 
   await prisma.battleLogs.create({
     data: {
